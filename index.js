baa0bce1c8b96fe0fd77201ae72e72e12f92bfb5
eohomegrownapps <euan.l.y.ong@gmail.com>
2020-09-20 12:03:26 +0100

    Updated web scraper

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const cheerio = require('cheerio');
 const rp = require('request-promise');
 
 const url = 'https://cors-anywhere.herokuapp.com/http://covid.gov.pk/';
-const epidata = 'http://storage.googleapis.com/static-covid/static/v4/balochistan/data-v4.json';
-const epidata_pk = 'http://storage.googleapis.com/static-covid/static/v4/balochistan/extdata-PK.json';
+const epidata = 'https://storage.googleapis.com/static-covid/static/v4/main/data-v4.json';
+const epidata_pk = 'https://storage.googleapis.com/static-covid/static/v4/main/extdata-PK.json';
 
-const group = "Default_Moderate";
-const name = "Expected spread (100%)";
+const group = "2W_Moderate";
+const key = "EXPECTED_WEAK";
 
 function getVal(cs,sel){
     var elem = cs.find(sel).next();
@@ -21,7 +21,7 @@ function getVal(cs,sel){
 
 function parseData(html){
     var $ = cheerio.load(html);
-    var cs = $('.covid-statistics');
+    var cs = $('.status');
 
     var result = {
         cases : {},
@@ -33,18 +33,18 @@ function parseData(html){
     
     //elem, text, name1, name2
     var elements = [
-        ["h4", "Confirmed Cases", "cases", "total"],
-        ["h6", "Cases (24 HRS)", "cases", "past24hrs"],
-        ["h6", "Deaths (24 HRS)", "deaths", "past24hrs"],
-        ["h6", "Tests (24 HRS)", "tests", "past24hrs"],
-        ["h6", "Total Tests", "tests", "total"],
-        ["h4", "Deaths", "deaths", "total"],
-        ["h4", "Recovered", "recovered", "total"],
-        ["h4", "Sindh", "cases", "sindh"],
-        ["h4", "Punjab", "cases", "punjab"],
-        ["h4", "Islamabad", "cases", "islamabad"],
-        ["h4", "Balochistan", "cases", "balochistan"],
-        ["h4", "KP", "cases", "kp"]
+        ["label", "Confirmed Cases", "cases", "total"],
+        //["h6", "Cases (24 HRS)", "cases", "past24hrs"],
+        //["h6", "Deaths (24 HRS)", "deaths", "past24hrs"],
+        //["h6", "Tests (24 HRS)", "tests", "past24hrs"],
+        ["label", "Total Tests", "tests", "total"],
+        ["label", "Deaths", "deaths", "total"],
+        ["label", "Recovered", "recovered", "total"],
+        ["h6", "SINDH", "cases", "sindh"],
+        ["h6", "PUNJAB", "cases", "punjab"],
+        ["h6", "ISLAMABAD", "cases", "islamabad"],
+        ["h6", "BALOCHISTAN", "cases", "balochistan"],
+        ["h6", "KPK", "cases", "kp"]
     ];
 
     for (var elem of elements){
@@ -52,11 +52,13 @@ function parseData(html){
     }
 
     //handle AJK and GB separately
-    result["cases"]["ajk"]=getVal(cs,'.ajk-stats:not(:contains("AJK"))');
-    result["cases"]["gb"]=getVal(cs,'.stats-gb:not(:contains("GB"))');
+    var ajkgb = cs.find('h6:contains("AJK/GB")').prev().text().split("/");
 
-    var datestr = cs.find("#date").text();
-    var arr = datestr.trim().match(/^LAST UPDATED AT: (\d{1,2}) (\w+), (\d{4}) - (\d{1,2}):(\d{1,2})(\w+)$/);
+    result["cases"]["ajk"] = parseInt(ajkgb[0].replace(/,/g, ''));
+    result["cases"]["gb"] = parseInt(ajkgb[1].replace(/,/g, ''));
+
+    var datestr = cs.find("#last-update").text();
+    var arr = datestr.trim().match(/^(\d{1,2}) (\w+), (\d{4}) - (\d{1,2}):(\d{1,2})(\w+) (\w+)\/(\w+)$/);
 
     if (arr[6].toLowerCase()=="am"){
         if (arr[4]=="12"){
@@ -79,7 +81,7 @@ function parseData(html){
                     var traceind = json_pk["models"]["date_index"].indexOf(date.toISOString().slice(0, 10))+1; //TODO this +1 is dodgy...
                     var traces = json_pk["models"]["traces"];
                     for (var i = 0; i<traces.length; i++){
-                        if (traces[i]["group"]==group && traces[i]["name"]==name){
+                        if (traces[i]["group"]==group && traces[i]["key"]==key){
                             result["estimated"]["cases"] = Math.round(json["regions"]["PK"]["Population"]*traces[i]["active"][traceind]);
                             break;
                         }
